73aad38e6f45e9bac8baae610c7b18c75a867a39
Nazmul457 <nazmulhassanbadhon12@gmail.com>
2023-11-06 19:30:12 +0600

    Category navigation bar added with some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CatnavigationComponent
   ],
   imports: [
     BrowserModule,
