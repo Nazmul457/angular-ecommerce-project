a87e705785d3f9b27773fe09c22d294e2a32a9eb
Nazmul457 <nazmulhassanbadhon12@gmail.com>
2023-11-06 23:12:13 +0600

    Fetching data into side navigation from a sample data file

--- src/app/home/components/sidenavigation/sidenavigation.component.ts
+++ src/app/home/components/sidenavigation/sidenavigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../types/category.type';
 
 @Component({
   selector: 'app-sidenavigation',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenavigation.component.scss']
 })
 export class SidenavigationComponent {
+  categories: Category[] = [];
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
 
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
 }
