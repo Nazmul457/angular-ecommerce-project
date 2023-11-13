7a2b2c526fd183deefb530644bfcb8a7b10619c5
Nazmul457 <nazmulhassanbadhon12@gmail.com>
2023-11-14 00:38:05 +0600

    environment variables added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseURL: 'https://angular-ecommerce-live-nazmul.onrender.com'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseURL: 'https://angular-ecommerce-live-nazmul.onrender.com'
+}
